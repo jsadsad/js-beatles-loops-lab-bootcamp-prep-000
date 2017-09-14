de157bc4b145ccec21fbe0d6b1f20d24447683f8
JCS <joshsad2@gmail.com>
2017-09-14 06:22:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function theBeatlesPlay(arrayMusicians, arrayInstruments) {
   }
   return array;
 }
+
+function johnLennonFacts(facts) {
+  var array = [];
+  let i = 0;
+
+  while ( i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++
+  }
+  return array;
+}
