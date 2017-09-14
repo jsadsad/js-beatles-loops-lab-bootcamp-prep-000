8f92d0001ac3b88a5c77eaf2945cad877606b82d
JCS <joshsad2@gmail.com>
2017-09-14 06:18:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 function theBeatlesPlay(arrayMusicians, arrayInstruments) {
   var array = [];
+
+  for (var i = 0; i < arrayMusicians.length; i++) {
+    var person = arrayMusicians[i];
+    array.push(`${arrayMusicians[i]} plays ${arrayInstruments}`);
+  }
+  return array;
 }
